90c4a728e6af28c4f2a6a358c6c86f9524fc86ab
Manoj Bharadwaj <mbharadwaj@gmail.com>
2020-11-01 13:40:40 -0500

    resolved heroku configuration error

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "general]
-email = \"mbharadwaj@gmail.com\"
+echo "\n\
+[general]\n\
+email = \"mbharadwaj@gmail.com\"\n\
+\n\
 " > ~/.streamlit/credentials.toml
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
